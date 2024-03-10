387f579c4383da2413a4e86065c29c28f5e61277
Arjita Singh Kushwaha <arjitakushwah@gmail.com>
2024-03-10 21:41:39 +0530

    insert at begin

--- Linked List/implementation.java
+++ Linked List/implementation.java
@@ -24,18 +24,47 @@ public class implementation {
             }
             tail=temp;
         }
+        //insert at beginning
+        void insertAtBegin(int val){
+            Node temp= new Node(val);
+            if(head==null){
+                head=temp;
+                tail=temp;
+            }
+            else{
+                temp.next=head;
+                head=temp;
+            }
+            
+        }
         //display the linked list
         void display(){
             Node temp= head;
             while (temp!=null) {
-                System.out.println(temp.data);
+                System.out.print(temp.data+" ");
                 temp = temp.next;
             }
+            System.out.println();
+        }
+
+        //length of linked list
+        int size(){
+            int count=0;
+            Node temp =head;
+            while(temp!=null){
+                count++;
+                temp=temp.next;
+            }
+            return count;
         }
     }
     public static void main(String[] args) {
         Linkedlist ll = new Linkedlist();
         ll.insertAtEnd(65);
+        ll.insertAtEnd(21);
+        ll.display();
+        ll.insertAtBegin(43);
+        ll.display();
 
     }
 }
